6841129a5467771204ab8483a272d73d8b31fa78
sally-bruen <sbruen@tcd.ie>
2024-02-25 20:27:34 +0000

    Removing comment section
    getting rid of code that doesn't work

--- 2TextMeasures.py
+++ 2TextMeasures.py
@@ -24,32 +24,6 @@ def calculate_measures(row):
         # Add more measures as needed
     }
 
-
-# def process_text_stats(input_file, output_file):
-#     with open(input_file, 'r', newline='') as input_file:
-#         reader = csv.DictReader(input_file, delimiter='\t')
-#
-#         with open(output_file, 'w', newline='') as output_file:
-#             fieldnames = ['FILENAME', 'TTR', 'WTR', 'CTTR', 'WDSEN']
-#             writer = csv.DictWriter(output_file, fieldnames=fieldnames, delimiter='\t')
-#             writer.writeheader()
-#
-#             # # Add an empty line after the headers
-#             # writer.writerow({})
-#
-#             for row in reader:
-#                 measures = calculate_measures(row)
-#                 writer.writerow(measures)
-#
-#                 # # Add an empty line after each file's data
-#                 # writer.writerow({})
-#
-#
-# if __name__ == "__main__":
-#     input_file = '/Users/sallybruen/PycharmProjects/TextPrograms/SS Results/SSTextStats.txt'
-#     output_file = '/Users/sallybruen/PycharmProjects/TextPrograms/SS Results/SSTextMeasures.txt'
-#     process_text_stats(input_file, output_file)
-
 def process_text_stats(input_file, output_file):
     df = pd.read_excel(input_file)
 
